cccac024dba372f9b9c356b026c348241b90f696
Palak <palak.desai4501@gmail.com>
2023-08-30 00:25:22 +0530

    Feature: Added Logo and Gave Some CSS Styling

--- fitness-app/src/components/Navbar.jsx
+++ fitness-app/src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <Stack>
+      <Link to="/">
+      <img src={Logo} alt="logo" style={{width: '48px', height: '48px', margin:'0 20px'}} />
+      </Link>
+    </Stack>
   )
 }
 
