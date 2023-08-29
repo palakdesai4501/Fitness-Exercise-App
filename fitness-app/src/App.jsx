36892fc88a69455f1d26e3abc155693646eb998d
Palak <palak.desai4501@gmail.com>
2023-08-29 17:06:31 +0530

    Feature: Added Files and Folders

--- fitness-app/src/App.jsx
+++ fitness-app/src/App.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <Box width="400px">
-      <navbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/exercise/:id" element={<ExerciseDetail />}/>
         <Route />
       </Routes>
+      <Footer />
     </Box>
   )
 }
