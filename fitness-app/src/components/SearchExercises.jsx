f44b0a99b40f3a25d0139ff746926e08aef6e6c4
Palak <palak.desai4501@gmail.com>
2023-08-31 20:52:34 +0530

    Feature: Changed Indentations

--- fitness-app/src/components/SearchExercises.jsx
+++ fitness-app/src/components/SearchExercises.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState }from 'react';
+import { Box, Button, Stack, TextField, Typography} from '@mui/material'
 
 const SearchExercises = () => {
   return (
-    <div>
-      SearchExercises
-    </div>
+   <Stack alignItems="center" mt="37px" justifyContent="center" p="20px"> 
+   <Typography fontWeight={700}>Awsome Exercises You <br /> Should Know</Typography>
+
+   </Stack>
   )
 }
 
