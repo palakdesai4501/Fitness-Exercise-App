4655c8331de962ba48cadff2872cd688cae7db64
Palak <palak.desai4501@gmail.com>
2023-08-31 21:23:20 +0530

    Feature: Added Button and Gave Styling Using sx property

--- fitness-app/src/components/SearchExercises.jsx
+++ fitness-app/src/components/SearchExercises.jsx
@@ -9,26 +9,40 @@ const SearchExercises = () => {
       justifyContent="center"
       p="20px">
       <Typography
-        fontWeight={700} sx={{fontSize: { lg: '44px', xs: '30px'}}}
+        fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }}
         mb="50px" textAlign="center">
         Awsome Exercises You <br /> Should Know
       </Typography>
-      <Box 
-      position="relative" 
-      mb="72px">
-        <TextField 
+      <Box
+        position="relative"
+        mb="72px">
+        <TextField
           sx={{
-            input: {fontWeight: '700', border: 'none', borderRadius: '4px'},
-            width: { lg: '1170px', xs: '350px'},
-            backgroundColor: '#fff', 
+            input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+            width: { lg: '800px', xs: '350px' },
+            backgroundColor: '#fff',
             borderRadius: '40px'
           }}
           height="76px"
           value=""
-          onChange={(e) => {}}
+          onChange={(e) => { }}
           placeholder="Search Exercises"
           type="text"
         />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: '#FF2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '12px'},
+            fontSize: { lg: '20px', xs: '14px'},
+            height: '56px',
+            position: 'absolute',
+            right: '0' //important for absolute positioning to make it at center
+          }}>
+          Search
+        </Button>
       </Box>
 
     </Stack>
