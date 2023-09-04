600708a14b521f726c3529195e4a0e22b822debe
Palak <palak.desai4501@gmail.com>
2023-09-04 22:49:47 +0530

    Feature: Added Deconstructing of Object

--- fitness-app/src/components/Detail.jsx
+++ fitness-app/src/components/Detail.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Typography, Stack, Button } from '@mui/material';
+import BodyPartImage from '../assets/icons/body-part.png';
+import TargetImage from '../assets/icons/target.png';
+import EquipmentImage from '../assets/icons/equipment.png';
 
-const Deail = () => {
+
+const Detail = ({exerciseDetail}) => {
+  const {bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
   return (
-    <div>
-      Detail
-    </div>
+    <Stack gap="60px" sx={{flexDirection: {lg: 'row'}, p: "20px", alignItems:"center"}}>
+      <img src={gifUrl} alt={name} Loading="lazy" className="detail-image" />
+
+      </Stack>
   )
 }
 
-export default Deail
+export default Detail
