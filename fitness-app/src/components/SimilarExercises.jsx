26165b610792d6a4b470a0c3177055d48a330d73
Palak <palak.desai4501@gmail.com>
2023-09-05 11:29:00 +0530

    Feature: Added Horizontal scrollbar

--- fitness-app/src/components/SimilarExercises.jsx
+++ fitness-app/src/components/SimilarExercises.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
-const SimilarExercises = () => {
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
-    <div>
-      SimilarExercise
-    </div>
+    <Box sx={{ mt: { lg: '100px', xs: '0' }}}>
+      <Typography variant='h3'>
+        Exercises that target the same muscle group
+      </Typography>
+      <Stack direction="row" sx={{ p: '2', position: 'relative' }}>
+        {targetMuscleExercises.length && <HorizontalScrollbar data={targetMuscleExercises} />}
+      </Stack>
+    </Box>
   )
 }
 
