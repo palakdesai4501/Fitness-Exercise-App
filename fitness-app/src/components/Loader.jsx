4ac00e6e9cb3bdea6f9cb21247b06a6c05fb9c23
Palak <palak.desai4501@gmail.com>
2023-09-05 11:32:05 +0530

    Feature: Added loader spinner dependency in json file

---
+++ fitness-app/src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+import { infinitySpin } from 'react-loader-spinner';
+
+const Loader = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Loader
