6f682424aed795874598a97d5036dfbcd1967992
Palak <palak.desai4501@gmail.com>
2023-09-06 09:52:35 +0530

    Feature: Changed Spelling Error

--- fitness-app/src/pages/ExerciseDetail.jsx
+++ fitness-app/src/pages/ExerciseDetail.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
-import Deail from '../components/Detail';
+import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 
@@ -37,7 +37,7 @@ const ExerciseDetail = () => {
 
   return (
     <Box>
-      <Deail exerciseDetail={exerciseDetail} />
+      <Detail exerciseDetail={exerciseDetail} />
       <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
       <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
     </Box>
