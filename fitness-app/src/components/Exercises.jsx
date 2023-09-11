ec4421ecc222a1da4fb41df5c12ee5f55275ac11
Palak <palak.desai4501@gmail.com>
2023-09-11 19:15:34 +0530

    Feature: Changed Color of Showing Results

--- fitness-app/src/components/Exercises.jsx
+++ fitness-app/src/components/Exercises.jsx
@@ -44,7 +44,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       mt="50px"
       p="20px"
     >
-      <Typography variant='h3' mb='46px'>
+      <Typography variant='h3' mb='46px' color='#fff'>
         Showing Results
       </Typography>
       <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px' } }}
