32bacae48996a8e03ec63128d670757c821338e4
Palak <palak.desai4501@gmail.com>
2023-09-12 14:48:30 +0530

    Feature: Changed Font Color to White

--- fitness-app/src/components/Detail.jsx
+++ fitness-app/src/components/Detail.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, Stack, Button } from '@mui/material';
+import { Typography, Stack, Button, colors } from '@mui/material';
 import BodyPartImage from '../assets/icons/body-part.png';
 import TargetImage from '../assets/icons/target.png';
 import EquipmentImage from '../assets/icons/equipment.png';
@@ -25,8 +25,8 @@ const Detail = ({ exerciseDetail }) => {
 
   return (
     <Stack gap="60px" sx={{ flexDirection: { lg: 'row' }, p: "20px", alignItems: "center" }}>
-      <img src={gifUrl} alt={name} Loading="lazy" className="detail-image" />
-      <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
+      <img src={gifUrl} alt={name} Loading="lazy" className="detail-image"/>
+      <Stack sx={{ gap: { lg: '35px', xs: '20px' }, color: '#fff' }}>
         <Typography variant='h3'>
           {name}
         </Typography>
