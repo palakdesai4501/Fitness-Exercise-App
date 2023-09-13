01cdc305f11ce559d40e9d36ce7b41f1a794891c
Palak <palak.desai4501@gmail.com>
2023-09-13 14:30:12 +0530

    Feature: Modified Navbar Elements

--- fitness-app/src/components/Navbar.jsx
+++ fitness-app/src/components/Navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = () => {
     sx={{gap:{sm:'122px', xs:'40px'}, mt:{sm:'32px', xs:'20px'}, justifyContent:'none'}} px="20px"
     >
       <Link to="/">
-        <img src={Logo} alt="logo" style={{ width: '50px', height: '50px', margin: '0 20px' }} />
+        <img src={Logo} alt="logo" style={{ width: '60px', height: '60px', margin: '0 20px' }} />
       </Link>
       <Stack
         direction="row"
         gap="40px"
-        fontSize="24px"
+        fontSize="25px"
         alignItems="flex-end">
         <Link to="/" className='nav-btn'
           style={{ textDecoration: 'none', color: "#fff"  }}>
